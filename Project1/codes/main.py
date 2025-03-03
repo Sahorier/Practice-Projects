22a816ae77a565d56faef22b0b07bb6b0d7e9aaa
Sahorier Nafiz <89661806+Sahorier@users.noreply.github.com>
2025-03-04 00:16:14 +0600

    update1

--- Project1/codes/main.py
+++ Project1/codes/main.py
@@ -10,7 +10,7 @@ electronics_cus = df[df['category'] == 'Electronics'] # Filtering the Electronic
 
 elec_cus_dict = {"age" : "total_spend"} # Creating a dictionary for electronics customers ages and total spneds
 
-for index, i in enumerate(list(electronics_cus['customer_age'])):
+for index, i in enumerate(list(electronics_cus['customer_age'])): # using for loop to marging multiple same ages in one key
     if str(i) in elec_cus_dict.keys():
         updated_value = elec_cus_dict[str(i)] + electronics_cus.iloc[index, 8]
         update = {str(i) : updated_value.round(2)}
@@ -18,11 +18,11 @@ for index, i in enumerate(list(electronics_cus['customer_age'])):
     else:
         elec_cus_dict[str(i)] = electronics_cus.iloc[index, 8]
     
-elec_single_age = list(set(electronics_cus['customer_age']))
+elec_single_age = list(set(electronics_cus['customer_age'])) # using set to sort the ages and remove multiple age
 elec_sorted_spend = list()
-for i in elec_single_age:
+for i in elec_single_age:  # using for loop to sort the spends as same as the ages for accurate plot
     value = elec_cus_dict[str(i)]
     elec_sorted_spend.append(value)
 
-plt.plot(elec_single_age,elec_sorted_spend)
+plt.plot(elec_single_age,elec_sorted_spend) # creating a plot that shows the ages and their spends
 plt.show()
